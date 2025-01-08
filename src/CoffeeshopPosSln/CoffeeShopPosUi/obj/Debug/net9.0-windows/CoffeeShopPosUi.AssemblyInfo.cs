12c518edade64859b1d7138a2008256af092899b
marwan <marwanachbani@gmail.com>
2025-01-08 15:30:14 +0100

    Implement Menu Management feature with ViewModel, View, and repository

--- src/CoffeeshopPosSln/CoffeeShopPosUi/obj/Debug/net9.0-windows/CoffeeShopPosUi.AssemblyInfo.cs
+++ src/CoffeeshopPosSln/CoffeeShopPosUi/obj/Debug/net9.0-windows/CoffeeShopPosUi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShopPosUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a98c0f3023c584e99aff3a6b8b59e9c05b281a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e174b50c188bf6ff36ceea0ce6b2154e52920e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShopPosUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShopPosUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
